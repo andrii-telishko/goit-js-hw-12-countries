ba033a1fc4dc211a321388428082de28bd68636e
Andrii Telishko <telishkoav@gmail.com>
2021-04-12 23:20:38 +0300

    add lib pnotify for errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import { debounce } from 'lodash';
 import './styles.css';
 import countriesList from './templates/countries-list.hbs'
 import countryCard from './templates/country-card.hbs'
-import '@pnotify/core/dist/BrightTheme.css';
-import { alert, notice, info, success, error} from '@pnotify/core';
+import { error, Stack } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import * as Confirm from "@pnotify/confirm";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
+
+
+
 
 
 console.log(countryCard);
@@ -22,12 +28,12 @@ console.log(refs.countriesField);
 console.dir(refs.input);
 
 function fetchCountries (name) {
-        return  fetch(`https:restcountries.eu/rest/v2/name/${name}`)
+        return  fetch(`https:restcountries.eu/rest/v2/name1/${name}`)
         .then(response => response.json())
     
 }
 
-refs.input.addEventListener('input', debounce(fetchCountriesList, 1000))
+refs.input.addEventListener('input', debounce(fetchCountriesList, 500))
 
 function fetchCountriesList(e) {
     const name = e.target.value
@@ -41,14 +47,44 @@ function fetchCountriesList(e) {
                 makeCountryCard(countries[0]);
             }
             else if (countries.length > 10) {
-                console.log('library');
+    error({
+  title: 'Oh No!',
+        text: 'Something terrible happened.',
+        stack: new Stack({
+    dir1: 'down',
+    modal: true,
+    firstpos1: 25,
+    overlayClose: false
+  }),
+  modules: new Map([
+      [
+        Confirm,
+        {
+          confirm: true,
+          buttons: [
+            {
+              text: "Ok",
+              primary: true,
+              click: notice => {
+                notice.close();
+              }
+            }
+          ]
+        }
+      ]
+    ])
+  });
+
+ 
+              
+                
             }
             else {
                 makeCountryMarkupList(countries)
                 
                 
             }
-        })
+        }).catch(() =>  refs.countriesField.innerHTML = '<p>Sorry! Can not find the page</p>')
     }
     
 
