192d5c9c4529bc461a3ed628b8944e1683c94e99
Andrii Telishko <telishkoav@gmail.com>
2021-04-14 19:53:44 +0300

    change url

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,8 @@
 
 
 export default (searchQuery) => {
-    // const BASE_URL = 'https:restcountries.eu/rest/v2/name';
-    return  fetch(`https:restcountries.eu/rest/v2/name/${searchQuery}`)
+    
+    return  fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
         .then(response => {
             if (!response.ok) {
                 throw error;
